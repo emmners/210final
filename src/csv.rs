84c1cc360211eb9fc427659d25e11b43d598ce53
emmners <iambigfruitsalad@gmail.com>
2025-05-04 03:55:36 +0000

    final commit

--- src/csv.rs
+++ src/csv.rs
@@ -1,62 +1,22 @@
-use serde::Deserialize;
-use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::File;
 use csv::ReaderBuilder;
+use crate::graph::ArrestRecord;
+use crate::graph::Graph;
 
-#[derive(Debug,Deserialize)]
-pub struct ArrestRecord {
-    #[serde(rename = "ARREST_PRECINCT")]
-    arrest_precinct: u32,
-    #[serde(rename = "OFNS_DESC")]
-    ofns_desc: String,
-}
+//this module is to read the csv and turn it into a graph using the graph functions in graph.rs.
 
-pub struct Graph {
-    precinct_frequency: HashMap<u32,HashMap<String,u32>>,
-    edge_weights: HashMap<(u32,u32),u32>,
-}
-
-impl Graph {
-    pub fn new() -> Self{
-        Self {
-            precinct_frequency: HashMap::new(),
-            edge_weights: HashMap::new(),
-        }
-    }
-    fn add_row(&mut self, precinct:u32, offense:String) {
-        let offense = offense.trim().to_uppercase();
-        let precinct_entry = self.precinct_frequency.entry(precinct).or_default();
-        let count = precinct_entry.entry(offense).or_insert(0);
-        *count += 1;
-    }
-
-    fn build_graph(&mut self) {
-        let precincts: Vec<u32> self.precinct_frequency.keys().cloned.collect();
-        for i in 0..precincts.len() {
-            for j in i + 1..precincts.len() {
-                let precinct1 = precincts[i];
-                let precinct2 = precincts[j];
-                let precinct1map = &self.precinct_frequency[&precinct1];
-                let precinct2map = &self.precinct_frequency[&precinct2];
-                
-                let sharedoffenses = Hash
-            }
-        }
-    }
-}
-
-pub fn read_csv(path: &str) -> Result<Vec<ArrestRecord>, Box<dyn Error>> {
+pub fn read_csv(path: &str) -> Result<Graph, Box<dyn Error>> {
     let file = File::open(path)?;
     let mut rdr = ReaderBuilder::new().from_reader(file);
-    let mut records = Vec::new();
+    let mut graph = Graph::new();
 
     for result in rdr.deserialize() {
         let record: ArrestRecord = result?;
-        if !record.ofns_desc.trim().is_empty() {
-            records.push(record);
-        }
+        if !record.pd_desc.trim().is_empty() {
+            graph.add_row(record.arrest_precinct,record.pd_desc);
+        } //for each row in the csv, create an arrestrecord that has the arrest precinct and pd_description, and add the row to a new graph
     }
 
-    Ok(records)
+    Ok(graph) 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+mod graph;
 mod csv;
 use crate::csv::read_csv;
+use crate::graph::Graph;
 
-fn main() {
+#[test] 
+fn test_disconnected_nodes() {
+    let mut graph = Graph::new();
+    graph.add_row(1, "THEFT".to_string());
+    graph.add_row(2, "ROBBERY".to_string());
+
+    graph.build_graph();
+    let clusters = graph.dfs(1);
+
+     assert_eq!(clusters.len(), 2);
+}
+
+fn main() { //read csv, build the graph, print edges, and print final clusters
     let path = "updated_NYPD_Arrest_Data_YTD.csv";
-    let records = read_csv(path);
-    println!("{:?}", records);
-    println!("Hello World");
+    let mut graph = read_csv(path).expect("reading failure");
+    graph.build_graph();
+    graph.print_weighted_edges();
+    graph.print_clusters();
 }
